feeebc02960be990ba4caeaf59828c1e47cfd5b5
And1210 <amf7crazy@gmail.com>
2019-02-02 13:44:36 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 @app.route("/")
 def main():
-    return "YEET"
+    data = '{"type": "Feature", "properties": { "title": "City Hall", "description": "0"}, "geometry": { "coordinates": [-79.384293, 43.653908 ], "type": "Point" }}'
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
