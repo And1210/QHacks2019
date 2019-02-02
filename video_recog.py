1ce91c51b0799b6e0b4989d002d91454c7b4a538
And1210 <amf7crazy@gmail.com>
2019-02-02 02:45:54 -0500

    We doing better with detection boisss

--- video_recog.py
+++ video_recog.py
@@ -14,32 +14,42 @@ with tf.Session() as sess:
     sess.graph.as_default()
     tf.import_graph_def(graph_def, name='')
 
-    # Read and preprocess an image.
-    ret, img = cap.read()
-    rows = img.shape[0]
-    cols = img.shape[1]
-    inp = cv2.resize(img, (300, 300))
-    inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
-
-    # Run the model
-    out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
-                    sess.graph.get_tensor_by_name('detection_scores:0'),
-                    sess.graph.get_tensor_by_name('detection_boxes:0'),
-                    sess.graph.get_tensor_by_name('detection_classes:0')],
-                   feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
-
-    # Visualize detected bounding boxes.
-    num_detections = int(out[0][0])
-    for i in range(num_detections):
-        classId = int(out[3][0][i])
-        score = float(out[1][0][i])
-        bbox = [float(v) for v in out[2][0][i]]
-        if score > 0.3:
-            x = bbox[1] * cols
-            y = bbox[0] * rows
-            right = bbox[3] * cols
-            bottom = bbox[2] * rows
-            cv2.rectangle(img, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=2)
-    cv2.imshow('TensorFlow MobileNet-SSD', img)
+    while (cap.isOpened()):
+        # Read and preprocess an image.
+        ret, img = cap.read()
+        if (ret == False):
+            break
+        rows = img.shape[0]
+        cols = img.shape[1]
+        inp = cv2.resize(img, (300, 300))
+        inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
+    
+        # Run the model
+        out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
+                        sess.graph.get_tensor_by_name('detection_scores:0'),
+                        sess.graph.get_tensor_by_name('detection_boxes:0'),
+                        sess.graph.get_tensor_by_name('detection_classes:0')],
+                       feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
+    
+        # Visualize detected bounding boxes.
+        num_detections = int(out[0][0])
+        for i in range(num_detections):
+            classId = int(out[3][0][i])
+            score = float(out[1][0][i])
+            bbox = [float(v) for v in out[2][0][i]]
+            if score > 0.3:
+                x = bbox[1] * cols
+                y = bbox[0] * rows
+                right = bbox[3] * cols
+                bottom = bbox[2] * rows
+                cv2.rectangle(img, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=2)
+        cv2.imshow('TensorFlow MobileNet-SSD', img)
+            
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cv2.waitKey()
+cv2.destroyAllWindows() 
+                
+                
