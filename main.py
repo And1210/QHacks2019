8454e18f7696624bc8af5cd65e62ef0c3dc60b24
And1210 <amf7crazy@gmail.com>
2019-02-02 16:29:36 -0500

    Lots of shit

--- main.py
+++ main.py
@@ -2,17 +2,61 @@ import numpy as np
 from video_recog import VideoRecognize
 from traffic_controller import TrafficController
 import mapbox_helper as mb
+from flask import Flask
+from flask_cors import CORS
+import random
 
+#Setting up video recognizer
 video = "rushhour.mp4"
 graph = "frozen_inference_graph.pb"
 videoGrab = VideoRecognize(video, graph)
 
-controller = TrafficController([2, 0, 2, 0])
+#Setting up traffic controller
+controller = [TrafficController([2*i%2, 2*((i+1)%2), 2*i%2, 2*((i+1)%2)]) for i in range(16)]
 
+#Setting up count arrays
 vehicleCounts = np.zeros(4)
 peopleCounts = np.zeros(4)
 
-out = mb.update('City Hall', '5')
+#Hardcoded intersections in Toronto
+longitudes = [43.659815, 43.660839, 43.661441, 43.659120, 43.651295, 43.652879, 43.657591, 43.658499, 43.647258, 43.648748, 43.650812, 43.650812, 43.646308, 43.648885, 43.649910, 43.650469]
+latitudes = [-79.390429, -79.385869, -79.383075, -79.382163, -79.405618, -79.397979, -79.389331, -79.384834, -79.404031, -79.396349, -79.386606, -79.386606, -79.391091, -79.385684, -79.380792, -79.378517]
+
+#Returns the GEOJSON for one feature
+def getFeature(title, state, lon, lat):
+    out = '{"type": "Feature", "properties": { "title": "' + str(title) + '", "description": "' + str(state) + '"}, "geometry": { "coordinates": [' + str(lat) + ', ' + str(lon) + '], "type": "Point" }}'
+    return out
+
+#Returns 
+def getFeatures():
+    out = '{"features":['
+    for i in range(len(longitudes)):
+        lon = longitudes[i]
+        lat = latitudes[i]
+        title = 'Feature' + str(i)
+        state = int(controller[i].getLights()[0]/2)
+        if (i < len(longitudes) - 1):
+            out = out + getFeature(title, state, lon, lat) + ', '
+        else:
+            out = out + getFeature(title, state, lon, lat)
+    out = out + '], "type": "FeatureCollection"}'
+    return out
+
+print(getFeatures())
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def main():
+    data = getFeatures()
+    return data
+#
+if __name__ == "__main__":
+    app.run(host="192.168.137.1", port=80)
+
+#out = mb.create('Test', '2', 44.6788, 43.8757).json()
+#out = mb.update('City Hall', '0')
 
 #videoGrab.startSess()
 #while (True):
--- mapbox_helper.py
+++ mapbox_helper.py
@@ -1,4 +1,5 @@
 from mapbox import Datasets
+import random as rand
 
 datasets = Datasets(access_token="sk.eyJ1IjoiYW5hbnRqIiwiYSI6ImNqcm5pa3NjbDA2cW8zeW8zb25nOTczY2sifQ.QkSZBaZnn6KxwO-YpRtA8A")
 
@@ -20,4 +21,15 @@ def update(name, value):
         return update
     else:
         return None
+    
+def create(name, value, long, lat):
+    curID = rand.getrandbits(32)
+    resp = datasets.create(name='features-example').json()
+    feature = {
+            'type': 'Feature', 'id': str(curID), 'properties': {'title': name, 'description': value}, 
+            'geometry': {'type': 'Point', 'coordinates': [long, lat]}}
+    resp = datasets.update_feature('cjrna6cc80dbo2wphvazyuf89', str(curID), feature)
+    return resp
+    
+    
     
